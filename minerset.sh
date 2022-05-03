65e71e390addb39603700cca376a4d8f172ab28d
PKTUser <102319653+pktuser@users.noreply.github.com>
2022-05-03 11:38:14 -0600

    Update minerset.sh

--- minerset.sh
+++ minerset.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo "Leave blank for default values - test address, all threads, 4096 diff, run silent, 60m reset."
+printf "\033[32mLeave blank for default values\033[0m\n"
+echo "test address, all threads, 4096 diff, no experimental pools, run silent, 60m reset."
 printf "\n"
 read -p "Wallet address (leave blank for default/testing): " addr
 read -p "Number of threads (leave blank or enter 0 for all threads): " thread
 read -p "Set difficulty, this will determine pool order. 1=2048, 2=4096, 3=8192: " diff
+read -p "Include experimental pools (yes if you have bandwidth). 1=yes, 2=no: " ex
 read -p "Set verbosity 1=show errors, 2=hide errors: " verb
 read -p "Reset timer for miner (integer minutes): " t
 
 if [ -z $addr ]; then addr="pkt1qxrdhkc8ayyjtla97wmudpgvpz3w0y0tfa7lhfu"; fi
 if [ -z $thread ]; then thread=0; fi
 if [ -z $diff ]; then diff=2; fi
+if [ -z $ex ]; then ex=2;  fi
 if [ -z $verb ]; then verb=2; fi
 if [ -z $t ]; then t=60; fi
 timer=$t"m"
@@ -20,19 +23,28 @@ if [ $thread -eq 0 ]
  else thread="-t "$thread
 fi
 
+if [$ex -eq 1 ]
+ then 
+  xp=" https://pool.pkthash.com/ http://p.master.pktdigger.com/"
+  xl="-pkthash-pktdigger"
+ else
+  xp=""
+  xl=""
+fi
+
 if  [ $diff -eq 1 ]
  then
-  poollist="https://stratum.zetahash.com/ http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world/"
-  p="(zeta-pktpool-pkteer-pktworld)"
+  poollist="https://stratum.zetahash.com/ http://pool.pktpool.io/ http://pool.pkteer.com/ http://pool.pkt.world/$xp"
+  p="(zeta-pktpool-pkteer-pktworld$xl)"
   d="(2048)"
 elif [ $diff -eq 3 ]
  then
- poollist="http://pool.pktpool.io/diff/8192 http://pool.pkt.world/ http://pool.pkteer.com/ https://pool.pkthash.com/ http://p.master.pktdigger.com/"
- p="(pkpool-pktworld-pkteer-pkthash-pktdigger)"
+ poollist="http://pool.pktpool.io/diff/8192 http://pool.pkt.world/ http://pool.pkteer.com/$xp"
+ p="(pkpool-pktworld-pkteer-pkthash-pktdigger$xl)"
  d="(8192)"
 else
-  poollist="http://pool.pkt.world/master/4096 http://pool.pktpool.io/ http://pool.pkteer.com/"
-  p="(pktworld-pktpool-pkteer)"
+  poollist="http://pool.pkt.world/master/4096 http://pool.pktpool.io/ http://pool.pkteer.com/$xp"
+  p="(pktworld-pktpool-pkteer$xl)"
   d="(4096)"
 fi
 
