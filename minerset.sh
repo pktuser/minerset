5cb0c9eba7a9f499c3b1f8c566596a9bb7a96c66
PKTUser <102319653+pktuser@users.noreply.github.com>
2022-05-04 10:44:47 -0600

    Update minerset.sh

--- minerset.sh
+++ minerset.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
+# a e s t h e t i c s 
+RED="\033[32m"
+DIM="\033[2m"
+CF="\033[0m"
 
-printf "\n\n\033[32mLeave blank for default values\033[0m\n"
-echo "test address, ~/packetcrypt, all threads, 4096 diff, no experimental pools, run silent, 60m reset."
+
+printf "\n\n${RED}Leave blank for default values$CF\n"
+echo "${DIM}test address, ../packetcrypt, all threads, 4096 diff, no experimental pools, run silent, 60m reset.$CF"
 printf "\n"
 read -p "Wallet address (leave blank for default/testing): " addr
-read -p "Type your ./path/to/packetcrypt eg ~/packetcrypt: " path
+read -p "Type your /path/to/packetcrypt eg ../packetcrypt: " path
 read -p "Number of threads (leave blank or enter 0 for all threads): " thread
 read -p "Set difficulty, this will determine pool order. 1=2048, 2=4096, 3=8192: " diff
-read -p "Include experimental pools (yes if you have bandwidth). 1=yes, 2=no: " ex
+read -p "Include experimental pools (yes if you have bandwidth). 1=yes, 2=no: " exp
 read -p "Set verbosity 1=show errors, 2=hide errors: " verb
 read -p "Reset timer for miner (integer minutes): " t
 
 if [ -z $addr ]; then addr="pkt1qxrdhkc8ayyjtla97wmudpgvpz3w0y0tfa7lhfu"; fi
-if [ -z $path ]; then path="~/packetcrypt"; fi
+if [ -z $path ]; then path="../packetcrypt"; fi
 
-#currently not working, keeps loooping in while
-while [  -f "$path" ]
+#check if path is valid
+while [ ! -f "$path" ]
  do
   clear
-  printf "\033[31m./path/to/packetcrypt as entered is not valid\033[0m\n\n"
-  echo "Path stored as: $path"
-  [ ! -f ~/packetcrypt ] && echo "test shows file does not exist" || echo "test shows file exists"
+  printf "$RED/path/to/packetcrypt as entered is not valid\n"
+  printf "Path stored as: $path\n"
+  printf "try: /home/username/packetcrypt$CF\n"
+  [ ! -f $path ] && echo "test shows file does not exist" || echo "test shows file exists"
   read -p "Please re-enter path: " path
 done
 
 if [ -z $thread ]; then thread=0; fi
 if [ -z $diff ]; then diff=2; fi
-if [ -z $ex ]; then ex=2;  fi
+if [ -z $exp ]; then exp=2;  fi
 if [ -z $verb ]; then verb=2; fi
 if [ -z $t ]; then t=60; fi
 timer=$t"m"
@@ -36,7 +42,7 @@ if [ $thread -eq 0 ]
  else thread="-t "$thread
 fi
 
-if [$ex -eq 1 ]
+if [ $exp -eq 1 ]
  then 
   xp=" https://pool.pkthash.com/ http://p.master.pktdigger.com/"
   xl="-pkthash-pktdigger"
@@ -53,7 +59,7 @@ if  [ $diff -eq 1 ]
 elif [ $diff -eq 3 ]
  then
  poollist="http://pool.pktpool.io/diff/8192 http://pool.pkt.world/ http://pool.pkteer.com/$xp"
- p="(pkpool-pktworld-pkteer-pkthash-pktdigger$xl)"
+ p="(pkpool-pktworld-pkteer$xl)"
  d="(8192)"
 else
   poollist="http://pool.pkt.world/master/4096 http://pool.pktpool.io/ http://pool.pkteer.com/$xp"
@@ -63,14 +69,14 @@ fi
 
 if [ $verb -eq 2 ]
  then
- verbosity=('2>&1 | grep --color=never -o "annmine.rs.*Ke.*"')
+ verbosity=("2>&1 | grep rs:525")
  v="silent"
  else
  verbosity=""
  v="verbose"
 fi
 
-mine="timeout $timer $path ann -p $addr $poollist $thread "${verbosity[@]}" "
+mine="timeout $timer $path ann -p $addr $poollist $thread $verbosity "
 echo "VARIABLES ARE NOW ALL SET"
 
 printf "\n"
